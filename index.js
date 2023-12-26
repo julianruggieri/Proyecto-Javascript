2ba3a41de92df2e17188e4e43e70774ece592370
julianruggieri <juliruggieri@hotmail.com>
2023-12-25 23:53:06 -0300

    avanzando 2da entrega

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 
 
 alert("¡Bienvenidx a la trivia de Caseros! \n ¡A jugar!")
-let puntos = 0
-
-opciones = parseInt(prompt("Elija una opción: \n 1-Jugar \n 2-Ver puntaje \n 3-Reiniciar puntaje \n 4-¡Pregunta Bonus! \n 5-Salir"))
 
 
+
+
+function Jugador (nombre, puntuacion)  {
+    this.nombre= nombre,
+    this.puntuacion= puntuacion
+}
+let listaJugadores = []
+
+
+
+
+
+
+
+
+opciones = parseInt(prompt("Elija una opción:\n 1-Añadir jugador \n 2-Jugar \n 3-Ver puntaje \n 4-Reiniciar puntaje \n 5-¡Pregunta Bonus! \n 6-Añadir un jugador \n 7-Ver tabla de jugadores \n 8-Salir"))
+
+
+
+
+    let jugador = new Jugador(prompt("Ingrese el nombre del jugador: "), 0)
+    listaJugadores.push(jugador)
+    
 
 
 
@@ -14,7 +34,7 @@ function primer_pregunta(){
     let respuesta1=(prompt("¿Cuál es el club más grande del oeste? \n a-Estudiantes de Caseros \n b- Deportivo Morón \n c-Almagro"))
 
     if (respuesta1 == "a"){
-        puntos ++
+        jugador.puntuacion++
         alert("¡respuesta correcta!")
     }
     else if(respuesta1 == "b" || respuesta1 == "c"){
@@ -31,7 +51,7 @@ function segunda_pregunta(){
     let respuesta2=(prompt("¿A qué partido pertenece Caseros? \n a-Morón \n b- La Matanza \n c-Tres de Febrero"))
 
     if (respuesta2 == "c"){
-        puntos ++
+        jugador.puntuacion++
         alert("¡respuesta correcta!")
     }
     else if(respuesta2 == "b" || respuesta2 == "a"){
@@ -49,7 +69,7 @@ function tercer_pregunta(){
     let respuesta3=(prompt("¿Cuál es el mejor barrio de Tres de Febrero? \n a-Santos lugares \n  b-Caseros \n c-Villa Lynch"))
 
     if (respuesta3 == "b"){
-        puntos ++
+        jugador.puntuacion++
         alert("¡respuesta correcta!")
     }
     else if(respuesta3 == "a" || respuesta3 == "c"){
@@ -66,7 +86,7 @@ function cuarta_pregunta(){
     let respuesta4=(prompt("¿Cuál es el apodo de Estudiantes de Caseros? \n a-El taladro \n  b-El matador \n c-El funebrero"))
 
     if (respuesta4 == "b"){
-        puntos ++
+        jugador.puntuacion++
         alert("¡respuesta correcta!")
     }
     else if(respuesta4 == "a" || respuesta4 == "c"){
@@ -82,7 +102,7 @@ function quinta_pregunta(){
     let respuesta5=(prompt("¿Cuál es el nombre del túnel al lado de la cancha de Estudiantes? \n a-Néstor Kirchner \n  b-Mauricio Macri \n c-Javier Milei"))
 
     if (respuesta5 == "a"){
-        puntos ++
+        jugador.puntuacion++
         alert("¡respuesta correcta!")
     }
     else if(respuesta5 == "c" || respuesta5 == "b"){
@@ -100,7 +120,7 @@ function quinta_pregunta(){
 function resetear(){
     pregunta_reset = prompt("¿Quieres reiniciar tus puntos? (si/no)")
     if (pregunta_reset == "si"){
-        puntos = 0
+        jugador.puntuacion = 0
         alert("Tus puntos fueron reiniciados exitosamente")
     }
     else if (pregunta_reset == "no"){
@@ -123,11 +143,11 @@ function pregunta_bonus(){
         let pregunta_bonus = prompt("¿Cuántos habitantes tiene Caseros? \n a-64.659 habitantes \n b-95.785 habitantes \n c- 121.523" )
         if(pregunta_bonus == "b"){
             alert("FELICIDADES, contestaste correctamente y duplicarás tus puntos")
-            puntos = puntos*2
+            jugador.puntuacion = jugador.puntuacion*2
         }
             else if(pregunta_bonus== "a"|| pregunta_bonus == "c"){
                 alert("Lo siento, respuesta incorrecta :(. Perderás todos tus puntos")
-                puntos = 0
+                jugador.puntuacion = 0
 
             }
         }
@@ -138,6 +158,8 @@ function pregunta_bonus(){
     }
 
     }
+
+
     
    
 
@@ -153,7 +175,18 @@ function jugar(){
 }
 
 function ver_puntos(){
-    alert("Tu puntaje actual es de " + puntos + " puntos")
+    alert("Tu puntaje actual es de " + jugador.puntuacion + " puntos")
+}
+
+function tabla_jugadores(){
+    console.log(listaJugadores)
+
+}
+
+function añadir_jugador(){
+    let jugador2 = new Jugador(prompt("Ingrese el nombre del jugador: "), 0)
+    listaJugadores.push(jugador2)
+    jugador2 = jugador
 }
 
 
@@ -161,28 +194,42 @@ function ver_puntos(){
 
 while(opciones !== 5){
     switch(opciones){
-       case 1:
+
+        case 1:
+            añadir_jugador()
+            break
+
+       case 2:
             jugar()
             break
 
-        case 2:
+        case 3:
             ver_puntos()
             break
 
-        case 3:
+        case 4:
             resetear()
             break
 
-        case 4:
+        case 5:
             pregunta_bonus()
             break
+        case 6:
+            añadir_jugador()
+            break
+
+        case 7:
+            tabla_jugadores()
+            break
+
+
 
         default:
         alert("opción no válida")
         break
 }
 
-opciones = parseInt(prompt("Elija una opción: \n 1-Jugar \n 2-Ver puntaje \n 3-Reiniciar puntaje \n 4-¡Pregunta Bonus! \n 5-Salir"))
+opciones = parseInt(prompt("Elija una opción:\n 1-Añadir jugador \n 2-Jugar \n 3-Ver puntaje \n 4-Reiniciar puntaje \n 5-¡Pregunta Bonus! \n 6-Añadir un jugador \n 7-Ver tabla de jugadores \n 8-Salir"))
     
     
 }
